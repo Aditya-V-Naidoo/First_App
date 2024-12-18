b951b8da7233a23f927af1e1638bfe031a790476
Aditya-V-Naidoo <aditya.v.naidoo@gmail.com>
2024-12-18 16:48:55 +0530

    updated templates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,21 @@
 class PagesController < ApplicationController
-  def testing
+  def login
 
   end
   def about
 
   end
+  def shopping_lists
+
+  end
+  def add_list
+
+  end
+  def delete_list
+
+  end
+  def update_list
+
+  end
+
 end
