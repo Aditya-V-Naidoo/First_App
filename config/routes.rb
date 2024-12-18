c1b31afe0f353a8ca8880444f8f40a73caf3341a
Aditya-V-Naidoo <aditya.v.naidoo@gmail.com>
2024-12-18 16:59:16 +0530

    modified new templates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ root 'pages#login'
 get 'about', to: 'pages#about' 
 get 'login', to: 'pages#login' 
 get 'list', to: 'pages#shopping_lists' 
-get 'add_list;', to: 'pages#add_list' 
+get 'add_list', to: 'pages#add_list' 
 get 'delete_list', to: 'pages#delete_list' 
 get 'update_list', to: 'pages#update_list' 
 end
